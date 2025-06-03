365dbc32b27437f68d25116eba82674b9814faef
rts-Liva <U2451012@rts-365.dk>
2025-06-03 14:01:58 +0200

    home screen

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router";
+
 function HomePage() {
+
     return (
-        <>
-            <h1>Home</h1>
-        </>
+        <main className="home">
+            <img src="/logo.png" alt="logo" className="home__logo" />
+            <h1 className="home__title">weather<span className="home__title--yellow">foreCasts</span></h1>
+            <Link to='/weather' className="home__link">get started</Link>
+        </main>
     );
 }
 
