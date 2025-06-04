5b17fcc153f3833f2761012732547265d96de4be
rts-Liva <U2451012@rts-365.dk>
2025-06-04 11:26:27 +0200

    3-hourly forecast

--- src/components/city.jsx
+++ src/components/city.jsx
@@ -1,6 +1,57 @@
+import { useEffect, useState } from "react";
 import { FaWater, FaWind } from "react-icons/fa";
 
 function City({ city }) {
+    const [forecasts, setForecasts] = useState([]);
+
+    const API_KEY = import.meta.env.VITE_API_KEY;
+
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    const currentMonth = currentDate.getMonth() + 1;
+    const currentDay = currentDate.getDay() + 1;
+
+    const months = [
+        'january',
+        'february',
+        'march',
+        'april',
+        'may',
+        'june',
+        'july',
+        'august',
+        'september',
+        'october',
+        'november',
+        'december'
+    ];
+
+    useEffect(() => {
+        async function weatherForecasts() {
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${city?.coord?.lat}&lon=${city?.coord?.lon}&appid=${API_KEY}&units=metric`);
+
+            if (response.ok) {
+                const data = await response.json();
+
+                setForecasts(data.list);
+            }
+        }
+
+        weatherForecasts();
+    }, [city]);
+
+    function isRightDay(date) {
+        const foreCastYear = Number(date.slice(0, 4));
+        const foreCastMonth = Number(date.slice(5, 7));
+        const foreCastDay = Number(date.slice(-2));
+
+        if (currentYear === foreCastYear && currentMonth === foreCastMonth && currentDay === foreCastDay) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     return (
         <section className="city">
             <h1 className="city__name">{city?.name}</h1>
@@ -23,6 +74,26 @@ function City({ city }) {
                 <div className="city__details-container">
                     <FaWind className="city__details-icon" />
                     <p className="city__text  city__text--lower">{city?.wind?.speed}m/s<br />Wind Speed</p>
+                </div>
+            </div>
+            <div className="city__forecast">
+                <div className="city__range city__range--heading">
+                    <p className="city__text">today</p>
+                    <p className="city__text">{`${currentDay}, ${months[currentMonth - 1]}`}</p>
+                </div>
+                <div className="city__range">
+                    {forecasts.map((forecast, index) => (
+                        isRightDay(forecast.dt_txt.slice(0, 10)) && (
+                            <div className="city__hour" key={index}>
+                                <p className="city__text">{forecast?.main?.temp.toFixed(0)}&#8451;</p>
+                                <img
+                                    src={`https://openweathermap.org/img/wn/${forecast?.weather[0]?.icon}@2x.png`}
+                                    alt="forecast icon"
+                                    className="city__forecast-icon" />
+                                <p className="city__text">{forecast?.dt_txt.slice(11, -3)}</p>
+                            </div>
+                        )
+                    ))}
                 </div>
             </div>
         </section>
